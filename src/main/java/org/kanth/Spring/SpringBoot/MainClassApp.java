a4b8349a205998a6035f4cae22693b74bdc4c85b
RamakanthB <kanth442@gmail.com>
2018-12-06 00:06:28 +0530

    spring boot start

---
+++ src/main/java/org/kanth/Spring/SpringBoot/MainClassApp.java
@@ -0,0 +1,13 @@
+package org.kanth.Spring.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClassApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainClassApp.class, args);
+	}
+
+}
